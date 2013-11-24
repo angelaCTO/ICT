2fa7cec213e7083012fbe2cf20155c2320516494
angelaCTO <to.angelac@gmail.com>
2013-11-23 22:37:58 -0800

    Update tiger_viewer.py

--- IPPaths/tiger_viewer.py
+++ IPPaths/tiger_viewer.py
@@ -5,36 +5,31 @@ import subprocess
 import sqlite3
 import argparse
 import datetime
-import cv, cv2
-import sys
-import re
 
-# parse the date input
-#parser = argparse.ArgumentParser()
-#parser.add_argument("date", help="input a date to search for video files")
-#args = parser.parse_args()
 
 def process_date(date):
    try:
         date = datetime.datetime.strptime(date,"%Y/%m/%d/%H")
         return True
-    except ValueError:
+   except ValueError:
         return False
 
+def prompt_date():
+    # Prompt for date
+    date = raw_input("\nPlease enter a date-time to be searched (yyyy/mm/dd/hh): ")
+    while (True):
+        processed = process_date(date)
+        if (processed):
+           print 'Searching date-time: ', date
+           return date
+        else:
+           date = raw_input("\nError processing date-time. Please re-enter date-time to be searched (yyyy/mm/dd/hh): ")       
 
-# Prompt for date
-date = raw_input("\nPlease enter a date-time to be searched (yyyy/mm/dd/hh): ")
-while (True):
-   processed = process_date(date)
-   if (processed):
-       print 'Searching date-time: ', date
-       break
-   else:
-       date = raw_input("\nError processing date-time. Please re-enter date-time to be searched (yyyy/mm/dd/hh): ")       
 
 
-# Get appropriate videos from tigerlog.db // Add paths to file: ex. vid_list.txt
-db = None
+
+date = prompt_date()
+
 try:
     print '\nOpening database to retrieve paths'
     tiger_db, cursor = log.open_data_base()     # in tiger_log.py
@@ -49,14 +44,15 @@ try:
     log.close_data_base(tiger_db)
     print 'Database closed successfully'
 
-    # Write date paths to vid_paths.txt
-    for path in dated:
-        with open(vid_paths.txt, 'w') as output:
-            output.write('file '+ path + '\n')    
 
 except sqlite3.Error, e:
     print "Error %s:" % e.args[0]
     sys.exit(1)
+
+# Write date paths to vid_paths.txt
+for path in dated:
+    with open(vid_paths.txt, 'w') as output:
+        output.write('file '+ path + '\n')    
 
 
 # Stitch toegether positve frames using ffmpeg
@@ -66,9 +62,4 @@ print 'Sucessful.'
 
 
 # Return stitched videos and play in viewer
-cap = cv2.VidCapture(vid_path)
-while cap.isOpened():
-    ret, frame = cap.read()
-    cv2.imshow('tiger_vid',frame)
-cap.release()
-cv2.destroyAllWindows()
+
